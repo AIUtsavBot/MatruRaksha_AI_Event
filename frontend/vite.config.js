96ebd95e5dad1df22237dee146e936ee761e80c7
KrishBhimani <erkrishbhimani@gmail.com>
2025-12-06 15:59:52 +0530

    dockerfiles added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,12 +6,13 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
-    cors: true
+    cors: true,
+    allowedHosts: ['.hello-xo.nl','matruraksha-frontend-xo-6hcx4uo.hello-xo.nl']
   },
   // ADD THE PREVIEW CONFIGURATION BLOCK HERE
   preview: {
     // Allows the specified host to access the preview server
-    allowedHosts: ['.hello-xo.nl']
+    allowedHosts: ['.hello-xo.nl','matruraksha-frontend-xo-6hcx4uo.hello-xo.nl']
   },
   build: {
     outDir: 'dist',
